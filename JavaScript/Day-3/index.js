67744793fc075ccc8be5403a6dea8e5e9d13e589
AyeshaMehboob <ayeshamehboobch@gmail.com>
2024-02-29 22:29:23 +0500

    Day-3-practice

---
+++ JavaScript/Day-3/index.js
@@ -0,0 +1,51 @@
+// Loops
+// for loop
+// While loop
+// do while loop
+
+// For Loop  -  for (initialization; condition; increment/ decrement) { // code to be executed }
+
+// for (let i = 0; i <= 10; i+=2){
+//     console.log(i);
+// }
+
+// let k = 0;
+// while(k <= 10) {
+//     console.log("hello world")
+//     k++;
+// }
+
+// j = 0;
+// do {
+//     console.log("hello people")
+//     j++;
+// } while( 0 < 0)
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i)
+// }
+
+//    Functions
+// function myfirstfunction(){
+//     alert("hello world");
+// }
+// myfirstfunction();
+
+// function mysecondfunction(){
+//     let num1 = 20;
+//     let num2 = 30;
+//     let result = num1 + num2;
+//     return result;
+// }
+
+// let output = mysecondfunction();
+// let result2 = output + 10;
+// alert(result2);
+
+
+// function mythirdfunction(name, age){
+    // alert("Hello " + name + " your age is " + age);   
+    // alert(`Hello ${name} your age is ${8}`)
+// }
+
+// mythirdfunction("Ajwah", 8);
---
+++ JavaScript/index.js
@@ -0,0 +1,46 @@
+// console.log(2+2);
+// document.write("hey there! ");
+// alert("helo");
+
+// Data Types
+
+// integers
+// Float
+// String
+// Boolean
+// undefined = undefined;
+// null = null;
+// Array
+
+// In array values shown by their indexes
+// array[0]= 1;  
+// array[1]=2
+
+// variables
+
+// var, let, const
+
+// In 'let' duplicates variables are not allowed
+// let name = "Ayesha";
+// let name = "Mehboob";
+
+// In 'var' we can use duplicates
+// var age = 20;
+// var age = 30;
+
+// In 'const' also duplicates are not allowed
+// const pi = 3.14;
+// const pi = 3.15;
+//         alert(pi);
+
+
+
+
+
+
+
+
+
+
+
+// perators
